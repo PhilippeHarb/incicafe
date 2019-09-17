4d12ece5184c6b24ab694faf922ff544e7aae052
snowpips <philippe.h99@gmail.com>
2019-09-17 12:47:24 +0300

    Add project files.

---
+++ incicafe.bll/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace incicafe.bll
+{
+    public class Class1
+    {
+    }
+}
---
+++ incicafe.dal/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace incicafe.dal
+{
+    public class Class1
+    {
+    }
+}
